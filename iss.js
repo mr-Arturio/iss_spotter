994ff639465e3c24370c97b8b0c4f11fd72c0c71
ArturT <mr.arturio@gmail.com>
2023-02-25 23:24:30 -0500

    refactoring

--- iss.js
+++ iss.js
@@ -8,18 +8,12 @@ const request = require('request');
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
 const fetchMyIP = function (callback) {
-  // use request to fetch IP address from JSON API
-  const API = 'https://api.ipify.org?format=json';
 
-  request(API, (error, response, body) => {
-    if (error) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
 
-      callback(error, null);
-      return;
-    }
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response ${body}`;
-      callback(Error(msg), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching IP. Response ${body}`), null);
       return;
     }
 
@@ -30,21 +24,26 @@ const fetchMyIP = function (callback) {
 
 };
 
+/**
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
+
 const fetchCoordsByIP = function (ip, callback) {
 
-  const API = `http://ipwho.is/${ip}`
-
-  request(API, (error, response, body) => {
-    if (error) {
-
-      callback(error, null);
-      return;
-    }
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) return callback(error, null);
 
     const parsedBody = JSON.parse(body);
+
     if (!parsedBody.success) {
-      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
-      callback(Error(message), null);
+      callback(Error(`Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`), null);
       return;
     }
 
@@ -54,24 +53,30 @@ const fetchCoordsByIP = function (ip, callback) {
 
   });
 };
+
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+
 const fetchISSFlyOverTimes = function (coords, callback) {
-  // use request to fetch IP address from JSON API
-  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`
 
-  request(url, (error, response, body) => {
-    if (error) {
+  request(`https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error) return callback(error, null);
 
-      callback(error, null);
-      return;
-    }
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching ISS past times: ${body}`;
-      callback(Error(msg), null);
-      return;
-    }
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS past times: ${body}`), null);
+  return;
+}
 
-    const passes = JSON.parse(body).response;
-    callback(null, passes);
+const passes = JSON.parse(body).response;
+callback(null, passes);
 
   });
 
